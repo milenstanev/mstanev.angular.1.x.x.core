0c6dd3dd4b8834113ca1e75672872db5b27152eb
Milen Stanev <milen.stanev@gmail.com>
2017-09-28 18:42:20 +0300

    Rebuild

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { default as angularAnimate } from 'angular/bower-angular-animate';
 import { default as toaster } from 'jirikavi/AngularJS-Toaster';
 import { default as ngStrap }  from 'angular-strap';
 import { default as CtrlBase } from './lib/CtrlBase.js';
-import { default as text } from 'text';
-import { default as css } from 'css';
-import { default as json } from 'json';
 
 const CoreModule = angular.module('mstanev.angular.core', [
   'ui.router',
@@ -26,8 +23,5 @@ export {
   angularAnimate,
   toaster,
   ngStrap,
-  CtrlBase,
-  text,
-  css,
-  json
+  CtrlBase
 }
