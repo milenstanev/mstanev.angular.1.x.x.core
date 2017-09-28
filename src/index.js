00749bba6f6228e47ef1c884564724de065a6c61
Milen Stanev <milen.stanev@gmail.com>
2017-09-28 18:50:04 +0300

    Add bundle deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {default as angular} from 'angular';
-import {default as uiRouter} from 'angular-ui-router';
-import {default as angularSanitize} from 'angular-sanitize';
-import {default as angularAnimate} from 'angular-animate';
-import {default as toaster} from 'jirikavi/AngularJS-Toaster';
-import {default as ngStrap}  from 'angular-strap';
-import { CtrlBase } from './lib/CtrlBase.js';
+import { default as angular } from 'angular/bower-angular';
+import { default as uiRouter } from 'angular-ui-router';
+import { default as angularSanitize } from 'angular/bower-angular-sanitize';
+import { default as angularAnimate } from 'angular/bower-angular-animate';
+import { default as toaster } from 'jirikavi/AngularJS-Toaster';
+import { default as ngStrap }  from 'angular-strap';
+import { default as CtrlBase } from './lib/CtrlBase.js';
 
 const CoreModule = angular.module('mstanev.angular.core', [
   'ui.router',
