deb2a367775822c42b41e00c393aa377769440cf
Milen Stanev <milen.stanev@gmail.com>
2017-09-29 20:57:40 +0300

    new way of deps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-export {default as angular} from 'angular/bower-angular';
-export { default as uiRouter } from 'angular-ui-router';
-export { default as angularSanitize } from 'angular/bower-angular-sanitize';
-export { default as angularAnimate } from 'angular/bower-angular-animate';
-export { default as toaster } from 'jirikavi/AngularJS-Toaster';
-export { default as ngStrap }  from 'angular-strap';
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'angular-sanitize';
+import 'angular-animate';
+import 'jirikavi/AngularJS-Toaster';
+import 'angular-strap';
+
+
+export {default as angular} from 'angular';
 export {default as CtrlBase} from './lib/CtrlBase.js';
 export const CoreModule = angular.module('mstanev.angular.core', [
   'ui.router',
