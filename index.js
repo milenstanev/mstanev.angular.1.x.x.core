36984ed31040cc2fc5ce6cca0325cfc30ecfd750
milenstanev <milen.stanev@gmail.com>
2017-09-30 19:55:33 +0300

    Test index in core

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-export * as dev from './milenstanev-mstanev.angular.1.x.x.core.dev.js';
-export * as prod from './milenstanev-mstanev.angular.1.x.x.core.js';
+/*export * as dev from './milenstanev-mstanev.angular.1.x.x.core.dev.js';
+export * as prod from './milenstanev-mstanev.angular.1.x.x.core.js';*/
+module.exports = require("./milenstanev-mstanev.angular.1.x.x.core.dev.js");
+module.exports = require("./milenstanev-mstanev.angular.1.x.x.core.js");
