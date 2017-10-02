b51e2ac45a7c61194c6b2cfb4126742e2a8c062b
milenstanev <milen.stanev@gmail.com>
2017-10-02 15:23:07 +0300

    Rebuilded.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,22 @@
-import SystemBuildConfig from './lib/SystemBuildConfig.js';
+export { default as SystemBuildConfig } from './lib/SystemBuildConfig.js';
 
 //region AngularJS + core modules about current application
-import { default as angular } from 'angular/bower-angular';
-import { default as uiRouter } from 'angular-ui-router';
-import { default as angularSanitize } from 'angular/bower-angular-sanitize';
-import { default as angularAnimate } from 'angular/bower-angular-animate';
-import { default as toaster } from 'jirikavi/AngularJS-Toaster';
-//import { default as ngStrap }  from 'angular-strap';
+export { default as angular } from 'angular/bower-angular';
+export { default as uiRouter } from 'angular-ui-router';
+export { default as angularSanitize } from 'angular/bower-angular-sanitize';
+export { default as angularAnimate } from 'angular/bower-angular-animate';
+export { default as toaster } from 'jirikavi/AngularJS-Toaster';
+//export { default as ngStrap }  from 'angular-strap';
 //endregion
 
 //region Helpers
-import { default as CtrlBase } from './lib/CtrlBase.js';
+export { default as CtrlBase } from './lib/CtrlBase.js';
 //endregion
 
-const CoreModule = angular.module('mstanev.angular.core', [
+export const CoreModule = angular.module('mstanev.angular.core', [
   'ui.router',
   'ngSanitize',
   'ngAnimate',
   'toaster'
   //, 'mgcrea.ngStrap'
 ]).name;
-
-
-class Test {
-  static $inject = () => [$http];
-}
-
-class Asd extends Test {}
-
-export {
-  CoreModule,
-  angular,
-  uiRouter,
-  angularSanitize,
-  angularAnimate,
-  toaster,
-  ngStrap,
-  CtrlBase,
-  SystemBuildConfig
-}
